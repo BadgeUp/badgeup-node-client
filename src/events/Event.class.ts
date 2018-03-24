27f40e0ff07d94f7674ffe4b5280a4074e5e1c9c
Mark Herhold <mark.herhold@gmail.com>
2018-03-24 01:56:38 +0000

    adding missing comments

--- src/events/Event.class.ts
+++ src/events/Event.class.ts
@@ -10,11 +10,34 @@ export interface IEventRequest {
 }
 
 export class EventRequest implements IEventRequest {
+    /**
+     * Arbitrary data that can be included to assist with achievement criteria evaluation.
+     */
     data?: any;
+
+    /**
+     * The metric key that will be modified as a result of this event.
+     */
     key: string;
+
+    /**
+     * Metric modifier key/value pair. Key may be one of @inc, @dec, @mult, @div, @set, @min, @max.
+     */
     modifier: IEventModifier;
+
+    /**
+     * Options that affect the state and operability of this event.
+     */
     options?: IEventOptions;
+
+    /**
+     * Uniquely identifies the subject the event is for.
+     */
     subject: string = '';
+
+    /**
+     * Event creation date/time string, represented in the ISO 8601 format. Timezones may be expressed with UTC offsets.
+     */
     timestamp?: Date;
 
     constructor(subject: string, key: string, modifier: IEventModifier = {}, options?: IEventOptions) {
@@ -24,14 +47,23 @@ export class EventRequest implements IEventRequest {
         this.options = options;
     }
 
+    /**
+     * Retrieves the event's modifier key
+     */
     get modifierKey() {
         return Object.keys(this.modifier)[0];
     }
 
+    /**
+     * Retrieves the event's modifier value
+     */
     get modifierValue() {
         return this.modifier[this.modifierKey];
     }
 
+    /**
+     * States if this event is set to be discarded (not persisted long-term)
+     */
     get discard() {
         if (this.options && this.options.discard === true) {
             return true;
@@ -42,7 +74,14 @@ export class EventRequest implements IEventRequest {
 }
 
 export interface IEventResponse extends IEventRequest {
+    /**
+     * A string that uniquely identifies this event. May be null if options.discard is set to true.
+     */
     id: string;
+
+    /**
+     * Unique application ID the event belongs to.
+     */
     applicationId: string;
 }
 
@@ -59,19 +98,31 @@ export interface IEventModifier {
     [key: string]: number;
 }
 
+/**
+ * Options that affect the state and operability of an event.
+ */
 export interface IEventOptions {
     discard: boolean;
 }
 
+/**
+ * Event response structure for the source event (API v1)
+ */
 export interface IEventResponseV1 {
     event: IEventResponse;
     progress: IProgress[];
 }
 
+/**
+ * Event response structure containing for the source or side-effect events (API v2)
+ */
 export interface IEventResponseV2Preview {
     results: IEventResponseResultV2Preview[];
 }
 
+/**
+ * Event response structure (API v2)
+ */
 export interface IEventResponseResultV2Preview {
     event: IEventResponse;
     cause: string;
