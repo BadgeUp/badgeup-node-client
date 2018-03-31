092b78e5c5f13a790d12676890d741d0f046f86e
Mark Herhold <mark.herhold@gmail.com>
2018-03-31 15:23:08 +0000

    fix parsing meta.created

--- src/http.ts
+++ src/http.ts
@@ -71,14 +71,13 @@ export class BadgeUpHttp {
                     const err = new Error(response.statusText);
                     return Promise.reject(err);
                 }
-                return response.text().then(responseText => {
-                    const f = (key, value) => {
-                        if (key === 'created') {
-                            return new Date(value);
-                        }
-                        return value;
-                    };
-                    return JSON.parse(responseText, f);
+                return response.json().then(responseBody => {
+                    const meta = responseBody.meta;
+                    if (meta && meta.created) {
+                        // parse ISO-8601 string to Date
+                        meta.created = new Date(meta.created);
+                    }
+                    return responseBody;
                 });
             });
     }
